fc97e07a4b201e81259ca0909205316b5b1d948d
Jared Hanson <jaredhanson@gmail.com>
2015-06-02 16:24:38 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# fingro-webfinger
---
+++ test/package.test.js
@@ -0,0 +1,11 @@
+/* global describe, it, expect */
+
+var pkg = require('..');
+
+describe('fingro-webfinger', function() {
+  
+  it('should export object', function() {
+    expect(pkg).to.be.an('object');
+  });
+  
+});
