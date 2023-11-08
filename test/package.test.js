61b231f6cc851f1bd64c68e57cdef4272f57f201
Jared Hanson <jaredhanson@gmail.com>
2023-11-08 15:52:12 -0800

    Filter services in resolve.

--- test/package.test.js
+++ test/package.test.js
@@ -14,7 +14,50 @@ describe('fingro-webfinger', function() {
   
   describe('resolve', function() {
     
-    it('should yield record when called without type argument', function(done) {
+    it('should yield record with service when service does not filter link relations', function(done) {
+      var webfinger = sinon.stub().yields(null, {
+        properties: {
+          'http://packetizer.com/ns/name#zh-CN': '保罗‧琼斯',
+          'http://packetizer.com/ns/activated': '2000-02-17T03:00:00Z',
+          'http://packetizer.com/ns/name': 'Paul E. Jones'
+        },
+        aliases: [ 'h323:paulej@packetizer.com' ],
+        links: [
+          { type: 'image/jpeg',
+            href: 'http://www.packetizer.com/people/paulej/images/paulej.jpg',
+            rel: 'http://webfinger.net/rel/avatar' },
+          { href: 'https://openid.packetizer.com/paulej',
+            rel: 'http://specs.openid.net/auth/2.0/provider' }
+        ],
+        subject: 'acct:paulej@packetizer.com'
+      });
+      
+      var resolver = $require('..', { webfinger: { webfinger: webfinger } })();
+      resolver.resolve('acct:paulej@packetizer.com', 'http://specs.openid.net/auth/2.0/provider', function(err, record) {
+        if (err) { return done(err); }
+        
+        expect(webfinger).to.have.been.calledOnce;
+        expect(webfinger).to.have.been.calledWith(
+          'acct:paulej@packetizer.com', 'http://specs.openid.net/auth/2.0/provider', { webfingerOnly: true }
+        );
+        expect(record).to.be.an('object');
+        expect(record).to.deep.equal({
+          subject: 'acct:paulej@packetizer.com',
+          aliases: [ 'h323:paulej@packetizer.com' ],
+          attributes: {
+            'http://packetizer.com/ns/name#zh-CN': '保罗‧琼斯',
+            'http://packetizer.com/ns/activated': '2000-02-17T03:00:00Z',
+            'http://packetizer.com/ns/name': 'Paul E. Jones'
+          },
+          services: [
+            { location: 'https://openid.packetizer.com/paulej', mediaType: undefined }
+          ]
+        });
+        done();
+      });
+    }); // should yield record with service when service does not filter link relations
+    
+    it('should yield record with all services when called without type argument', function(done) {
       var webfinger = sinon.stub().yields(null, {
         properties: {
           'http://packetizer.com/ns/name#zh-CN': '保罗‧琼斯',
@@ -60,7 +103,7 @@ describe('fingro-webfinger', function() {
         });
         done();
       });
-    }); // should yield record when called without type argument
+    }); // should yield record with all services when called without type argument
     
   }); // resolve
   
