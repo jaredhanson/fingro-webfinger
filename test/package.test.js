19d10f2052a76830ad24fd2f6b8c5b087ca149d3
Jared Hanson <jaredhanson@gmail.com>
2023-11-08 16:16:04 -0800

    Better no service handling.

--- test/package.test.js
+++ test/package.test.js
@@ -145,6 +145,40 @@ describe('fingro-webfinger', function() {
       });
     }); // should yield record with all services when called without type argument
     
+    it('should yield record without services when called without type argument', function(done) {
+      var webfinger = sinon.stub().yields(null, {
+        properties: {
+          'http://packetizer.com/ns/name#zh-CN': '保罗‧琼斯',
+          'http://packetizer.com/ns/activated': '2000-02-17T03:00:00Z',
+          'http://packetizer.com/ns/name': 'Paul E. Jones'
+        },
+        aliases: [ 'h323:paulej@packetizer.com' ],
+        subject: 'acct:paulej@packetizer.com'
+      });
+      
+      var resolver = $require('..', { webfinger: { webfinger: webfinger } })();
+      resolver.resolve('acct:paulej@packetizer.com', function(err, record) {
+        if (err) { return done(err); }
+        
+        expect(webfinger).to.have.been.calledOnce;
+        expect(webfinger).to.have.been.calledWith(
+          'acct:paulej@packetizer.com', undefined, { webfingerOnly: true }
+        );
+        expect(record).to.be.an('object');
+        expect(record).to.deep.equal({
+          subject: 'acct:paulej@packetizer.com',
+          aliases: [ 'h323:paulej@packetizer.com' ],
+          attributes: {
+            'http://packetizer.com/ns/name#zh-CN': '保罗‧琼斯',
+            'http://packetizer.com/ns/activated': '2000-02-17T03:00:00Z',
+            'http://packetizer.com/ns/name': 'Paul E. Jones'
+          },
+          services: {}
+        });
+        done();
+      });
+    }); // should yield record without services when called without type argument
+    
     it('should yield error when querying for unsupported service from service that filters link relations', function(done) {
       var webfinger = sinon.stub().yields(null, {
         properties: {
